56f4c593c06dc49e858bebbb41fe8fcec27a4e55
Arunkumar Nattamai Hariharan <anattama@redhat.com>
2022-09-08 17:38:03 -0400

    Updated Qty to 200

--- src/main/java/com/redhat/coolstore/InventoryResource.java
+++ src/main/java/com/redhat/coolstore/InventoryResource.java
@@ -42,7 +42,7 @@ public class InventoryResource {
         return Inventory.<Inventory>streamAll()
         .map( p -> {
             if("Raleigh".equalsIgnoreCase(p.location)) {
-                p.quantity = 100;
+                p.quantity = 200;
             } 
            return p;
         }) 
